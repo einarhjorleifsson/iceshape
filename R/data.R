d30b039aa8d4b48eba21b6bffee20ec0b558dcce
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2017-10-06 10:20:12 +0000

    tidying up

--- R/data.R
+++ R/data.R
@@ -7,21 +7,48 @@
 #'
 #' @format A data frame with 235 rows and 14 variables:
 #' \describe{
-#'   \item{fid}{the id}
-#'   \item{f_level}{xxx}
-#'   \item{f_code}{xxx}
-#'   \item{f_status}{xxx}
-#'   \item{ocean}{xxx}
-#'   \item{subocean}{xxx}
-#'   \item{f_area}{xxx}
-#'   \item{f_subarea}{xxx}
-#'   \item{f_division}{xxx}
-#'   \item{f_subdivis}{xxx}
-#'   \item{f_subunit}{xxx}
-#'   \item{surface}{xxx}
-#'   \item{name}{xxx}
-#'   \item{geometry}{xxx}
+#'   \item{name}{Name of the fao "area"}
+#'   \item{geometry}{Polygons as simple features}
 #' }
 #'
 #' @source \url{http://www.fao.org/figis/geoserver/area/ows?service=WFS&request=GetFeature&version=1.0.0&typeName=area:FAO_AREAS&outputFormat=SHAPE-ZIP}
 "fao"
+
+#' faolink
+#'
+#' A "lookup" table that links the fao unit of interest with the corresponding
+#' geometry names
+#'
+#' @format A data frame with 235 rows and 14 variables:
+#' \describe{
+#'   \item{unit}{Name of region of interest}
+#'   \item{name}{Name of the geometries that cover the region of interest}
+#' }
+#'
+"faolink"
+
+#' Bormicon areas
+#'
+#' A spatial dataset, name and geometry of bormicon areas
+#'
+#' @format A data frame with 16 rows and 2 variables:
+#' \describe{
+#'   \item{group}{the id}
+#'   \item{geometry}{xxx}
+#' }
+
+
+#' eez
+#'
+#' A spatial dataset, name and geometry of global eez
+#'
+#' @format A data frame with 281 rows and 24 variables:
+#' \describe{
+#'   \item{polygonid}{xxx}
+#'   \item{mrgid}{xxx}
+#' }
+#'
+#' @source Marine Regions, EEZ v9, low resolution
+#' \url{http://www.marineregions.org/downloads.php}
+#'
+"eez"
